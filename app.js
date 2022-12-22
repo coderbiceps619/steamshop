ff1aba1758a8bd049709aa3702d93980ba1e7ecf
Manu  Gupta <48170669+coderbiceps619@users.noreply.github.com>
2022-12-22 07:41:28 -0800

    Added mongo uri from env

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 const helmet = require('helmet');
 const stripe = require('stripe')('sk_test_bR91izX4vLiwfnpmznILatUl00CJtWAT55');
 
-const MongoDBuri = 'mongodb+srv://coderbiceps:HPWtVI3Q90ACMn7A@cluster0-wqpap.mongodb.net/shop'
+const MongoDBuri = process.env.MONGO_URL;
 
 const store = new MongoDBStore({
     uri : MongoDBuri ,
